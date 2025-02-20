de4fb056e7a394b0d890ce2c736e5812c2bbfac5
fany-santos8 <estefany_fanysantos@hotmail.com>
2025-02-19 23:23:05 -0300

    feat: configuração inicial do projeto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="max-w-7xl flex flex-col items-center font-quicksand text-5xl font-bold drop-shadow-lg transition-transform transform hover:scale-105 animate-pulse">Home</div>
+    <div className="max-w-7xl flex flex-col items-center ">Home</div>
   )
 }
 
